1b85fdccffe8cf56291e569c252b70abcdd29308
Martijn van der Elsen <m.vanderelsen@evebury.com>
2026-03-25 10:59:57 +0100

    improved schema memory footprint

--- Evebury.Gs1.Message/Xml/Schema/XmlSchemaSet.cs
+++ Evebury.Gs1.Message/Xml/Schema/XmlSchemaSet.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.IO;
 using System.Resources;
 using System.Text;
+using System.Xml;
 
 namespace Evebury.Gs1.Message.Xml.Schema
 {
@@ -24,30 +25,63 @@ namespace Evebury.Gs1.Message.Xml.Schema
                 System.Xml.Schema.XmlSchema schema = LoadSchema(resourceManager, resourceKey);
                 set.Add(schema);
             }
+            set.Compile();
             return set;
         }
 
         private static System.Xml.Schema.XmlSchema LoadSchema(ResourceManager resourceManager, string resourceKey)
         {
             System.Xml.Schema.XmlSchema schema = null;
+
+            XmlDocument xsd = null;
+
             object value = resourceManager.GetObject(resourceKey);
-            if (value is string xsd)
+            if (value is byte[] bytes)
             {
-                byte[] data = Encoding.UTF8.GetBytes(resourceManager.GetString(resourceKey));
-                using (MemoryStream ms = new(data))
-                {
-                    schema = System.Xml.Schema.XmlSchema.Read(ms, null);
-                }
+                xsd = LoadXmlSchema(bytes);
             }
-            else if (value is byte[] bytes)
+            else if (value is string str)
             {
-                using (MemoryStream ms = new(bytes))
+                xsd = LoadXmlSchema(Encoding.UTF8.GetBytes(str));
+            }
+
+            using (MemoryStream stream = new())
+            {
+                xsd.Save(stream);
+                stream.Position = 0;
+                schema = System.Xml.Schema.XmlSchema.Read(stream, null);
+            }
+            return schema;
+        }
+
+        private static XmlDocument LoadXmlSchema(byte[] bytes, bool skipAnnotations = true) 
+        {
+            XmlReaderSettings settings = new()
+            {
+                IgnoreComments = true,
+                IgnoreWhitespace = true,
+            };
+            XmlDocument xsd = new();
+
+            using (MemoryStream stream = new(bytes))
+            {
+                using (XmlReader reader = XmlReader.Create(stream, settings))
                 {
-                    schema = System.Xml.Schema.XmlSchema.Read(ms, null);
+                    xsd.Load(reader);
                 }
             }
 
-            return schema;
+            if (skipAnnotations)
+            {
+                XmlNamespaceManager xnm = new(xsd.NameTable);
+                xnm.AddNamespace("xsd", xsd.DocumentElement.NamespaceURI);
+                XmlNodeList nodes = xsd.SelectNodes("//xsd:annotation", xnm);
+                foreach (XmlNode node in nodes)
+                {
+                    node.ParentNode.RemoveChild(node);
+                }
+            }
+            return xsd;
         }
 
     }
